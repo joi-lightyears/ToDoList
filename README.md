68a908188825baa80ccee3462768d798e3e943bc
joi-lightyears <kunthanhdat@gmail.com>
2022-12-29 01:17:07 +0700

    project init

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ToDoLis
+# ToDoList
---
+++ project/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PageTitle from './components/PageTitle';
+function App() {
+  return (
+    <div className="container">
+      <PageTitle/>
+    </div>
+  );
+}
+
+export default App;
